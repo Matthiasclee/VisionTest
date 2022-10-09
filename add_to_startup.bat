77fbfc38c257471bcc30ab91a181e386cc9c5931
Matthiasclee <matthias@matthiasclee.com>
2022-10-09 10:15:25 -0700

    Fixed startup file

--- add_to_startup.bat
+++ add_to_startup.bat
@@ -1,2 +1,2 @@
-echo "cd %userprofile%\Desktop" > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\vt.bat"
-echo "ruby main.rb" >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\vt.bat"
+echo cd %userprofile%\Desktop > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\vt.bat"
+echo ruby main.rb >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\vt.bat"
