69e70ea91a4a1580cefe253422b2b8d7eb9f8c56
Matthias Lee <matthias@matthiasclee.com>
2022-10-30 16:23:54 -0700

    Fix typo

--- ser_port.rb
+++ ser_port.rb
@@ -1,3 +1,3 @@
-$ser_port = ENV["reciever_port"] ? ENV["receiver_port"] : (puts "Error";exit)
+$ser_port = ENV["receiver_port"] ? ENV["receiver_port"] : (puts "Error";exit)
 # UNCOMMENT FOR WINDOWS: 
 # $ser_port = 'COM3'
