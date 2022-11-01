e3ebc76249062726c5a1c4f8e3d85745052aa8d9
Matthias Lee <matthias@matthiasclee.com>
2022-11-01 06:25:23 -0700

    Calibration can be done with remote

--- main.rb
+++ main.rb
@@ -7,6 +7,15 @@ driver.navigate.to 'file://' + Dir.pwd + '/index.html'
 
 calibrationmode = false
 
+def changePxSize(num, driver)
+  lines = File.readlines('js/settings.js')
+  size_line = lines[1].split(" ")
+  size = size_line[2].to_i
+  lines[1] = "  localStorage.pxsize = #{size + num}\n"
+  File.write('js/settings.js', lines.join)
+  driver.execute_script size + num > size ? 'upPxSize()' : 'downPxSize()'
+end
+
 oncode '1B' do; driver.execute_script 'fill(cSize, true)' end
 oncode '03' do; driver.execute_script 'up()' end
 oncode '04' do; driver.execute_script 'down()' end
@@ -14,8 +23,8 @@ oncode '17' do; driver.execute_script 'up()' end
 oncode '1F' do; driver.execute_script 'down()' end
 oncode '1C' do; driver.execute_script 'up()' end
 oncode '1A' do; driver.execute_script 'down()' end
-oncode '05' do; driver.execute_script calibrationmode ? 'upPxSize()' : 'up()' end
-oncode '06' do; driver.execute_script calibrationmode ? 'downPxSize()' : 'down()' end
+oncode '05' do; calibrationmode ? changePxSize(1, driver) : driver.execute_script('up()') end
+oncode '06' do; calibrationmode ? changePxSize(-1, driver) : driver.execute_script('down()') end
 oncode '07' do; driver.execute_script 'tglLine()' end
 oncode '08' do; driver.execute_script 'tglSingle()' end
 oncode '09' do; driver.execute_script 'tglSizeNum()' end
