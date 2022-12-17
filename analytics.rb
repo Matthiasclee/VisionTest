5fb5bdd3aeb0b6ed89bcc501e34086fe09684bf1
Matthias Lee <matthias@matthiasclee.com>
2022-12-17 09:23:36 -0800

    Moved analytics to separate file

---
+++ analytics.rb
@@ -0,0 +1,7 @@
+def record_keypress_analytics(key)
+  analytics = JSON.parse(File.read("analytics/keypresses.json"))
+  analytics[key] = 0 if !analytics[key]
+  analytics[key] = analytics[key] + 1
+  File.write("analytics/keypresses.json", analytics.to_json, mode: "w")
+end
+
--- main.rb
+++ main.rb
@@ -1,12 +1,6 @@
-
-def record_keypress_analytics(key)
-  analytics = JSON.parse(File.read("analytics/keypresses.json"))
-  analytics[key] = 0 if !analytics[key]
-  analytics[key] = analytics[key] + 1
-  File.write("analytics/keypresses.json", analytics.to_json, mode: "w")
-end
 require_relative 'serial.rb'
 require 'selenium-webdriver'
+require_relative 'analytics.rb'
 require 'json'
 
 if !File.directory?("./analytics")
