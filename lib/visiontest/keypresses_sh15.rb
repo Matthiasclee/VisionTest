66c9e0d4b00f61898c68a665d16f4204717df45c
Matthias Lee <matthias@matthiasclee.com>
2022-12-22 10:23:53 -0800

    Added mode controls

--- lib/visiontest/keypresses_sh15.rb
+++ lib/visiontest/keypresses_sh15.rb
@@ -1,11 +1,13 @@
 module VisionTest
   module KeypressesSH15
+    cmode = :ltr
+
     # Size controls
     Serial.oncode 'A2' do; FirefoxCtrl.driver.execute_script 'up()' end
     Serial.oncode 'A6' do; FirefoxCtrl.driver.execute_script 'down()' end
 
-    # Mode button
-    Serial.oncode 'A1' do; FirefoxCtrl.driver.execute_script 'tglMode()' end
+    # Hide button
+    Serial.oncode 'A1' do; FirefoxCtrl.driver.execute_script 'tglSizeNum()' end
 
     # Screensaver
     Serial.oncode 'A0' do; FirefoxCtrl.driver.execute_script 'screenSaver()' end
@@ -13,5 +15,24 @@ module VisionTest
     # Randomize letters
     Serial.oncode 'A4' do; FirefoxCtrl.driver.execute_script 'fill(cSize, true)' end
 
+    # Mode buttons
+    Serial.oncode 'A3' do
+        FirefoxCtrl.driver.execute_script "
+          if (mode == 'letter'){nmode = 'e'};
+          if (mode == 'number'){nmode = 'letter'};
+          if (mode == 'symbol'){nmode = 'number'};
+          if (mode == 'e'){nmode = 'symbol'};
+          while (mode != nmode) {tglMode()};
+        "
+    end
+    Serial.oncode 'A5' do
+        FirefoxCtrl.driver.execute_script "
+          if (mode == 'letter'){nmode = 'number'};
+          if (mode == 'number'){nmode = 'symbol'};
+          if (mode == 'symbol'){nmode = 'e'};
+          if (mode == 'e'){nmode = 'letter'};
+          while (mode != nmode) {tglMode()};
+        "
+    end
   end
 end
