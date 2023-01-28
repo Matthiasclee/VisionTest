3a9c9982d536476246ff364fb0d72d8c3975754b
Matthias Lee <matthias@matthiasclee.com>
2023-01-28 09:14:04 -0800

    Removed settings.rb

--- lib/visiontest/settings.rb
+++
@@ -1,7 +0,0 @@
-# System settings
-
-$mode = :unix # Change to `:win` on windows machine
-if $mode == :win
-  STDOUT.puts "WARN: VisionTest no longer supports Windows systems, so some features may not work properly."
-end
-$windows_serial_port = 'COM4' # Change to appropriate serial port if on windows, disregard if not
