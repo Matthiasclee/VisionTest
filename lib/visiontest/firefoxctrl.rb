5d35743449de6555f8bb8645f466bb142d118384
Matthias Lee <matthias@matthiasclee.com>
2023-02-04 17:07:01 -0800

    FirefoxCtrl doesn't check for windows

--- lib/visiontest/firefoxctrl.rb
+++ lib/visiontest/firefoxctrl.rb
@@ -2,7 +2,6 @@ module VisionTest
   module FirefoxCtrl
     @driver=nil
     def self.start_driver
-      Selenium::WebDriver::Firefox::Service.driver_path = 'setup_files/geckodriver.exe' if $mode == :win
       options = Selenium::WebDriver::Firefox::Options.new(args: ['--kiosk'])
       @driver = Selenium::WebDriver.for :firefox, capabilities: options
       @driver.navigate.to 'file://' + ROOT_DIR + '/table.html'
