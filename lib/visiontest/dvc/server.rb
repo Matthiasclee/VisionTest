96b1033b3b3439fda73b2c43782121230b5bceed
Matthias Lee <matthias@matthiasclee.com>
2023-02-05 11:34:07 -0800

    DVC has its own password and id_server

--- lib/visiontest/dvc/server.rb
+++ lib/visiontest/dvc/server.rb
@@ -2,10 +2,10 @@ module VisionTest
   module DVC
     module Server
       def self.client_handler(client)
-        password_sha256 = File.read("#{ROOT_DIR}/lib/visiontest/vtac/pw_sha256").chomp
+        password_sha256 = File.read("#{ROOT_DIR}/lib/visiontest/dvc/pw_sha256").chomp
 
         puts 1
-        client.puts Packet.new(:id_server, File.read("#{ROOT_DIR}/lib/visiontest/vtac/id_server").chomp + "~v#{::VisionTest.version}" + "#{"~authreq" if (password_sha256 != "")}")
+        client.puts Packet.new(:id_server, File.read("#{ROOT_DIR}/lib/visiontest/dvc/id_server").chomp + "~v#{::VisionTest.version}" + "#{"~authreq" if (password_sha256 != "")}")
         packet = Packet.new(from_packet: client.gets)
         if packet[:type] == "id_client"
           if password_sha256 != ""
