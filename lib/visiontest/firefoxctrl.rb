cda15a11776b9b8a48a0aa0bbb0942a29abf4164
Matthias Lee <matthias@matthiasclee.com>
2023-02-04 16:44:14 -0800

    Renamed index.html

--- lib/visiontest/firefoxctrl.rb
+++ lib/visiontest/firefoxctrl.rb
@@ -5,7 +5,7 @@ module VisionTest
       Selenium::WebDriver::Firefox::Service.driver_path = 'setup_files/geckodriver.exe' if $mode == :win
       options = Selenium::WebDriver::Firefox::Options.new(args: ['--kiosk'])
       @driver = Selenium::WebDriver.for :firefox, capabilities: options
-      @driver.navigate.to 'file://' + ROOT_DIR + '/index.html'
+      @driver.navigate.to 'file://' + ROOT_DIR + '/table.html'
     end
 
     attr_reader :driver
