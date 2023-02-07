2e68023e59baad5c193836cc3b9ca0ddd638a6f0
Matthias Lee <matthias@matthiasclee.com>
2023-02-06 16:16:45 -0800

    All git commands use --force

--- lib/visiontest/vtac/commands.rb
+++ lib/visiontest/vtac/commands.rb
@@ -107,21 +107,14 @@ module VisionTest
             `shutdown -P 0`
             return Packet.new(:disconnect, "Shut down")
           elsif cmd == "update"
-            if args[0] == "force"
-              out = `cd #{ROOT_DIR};git pull --force`
-              return Packet.new(:response, out)
-            elsif args[0]
-              return Packet.new(:error, "Invalid command")
-            end
-
-            out = `cd #{ROOT_DIR};git pull`
+            out = `cd #{ROOT_DIR};git pull --force`
             return Packet.new(:response, out)
           elsif cmd == "version"
             if args[0] == "master"
-              out = `cd #{ROOT_DIR};git checkout master`
+              out = `cd #{ROOT_DIR};git checkout master --force`
               return Packet.new(:response, out)
             elsif args[0]
-              out = `cd #{ROOT_DIR};git checkout tags/v#{args[0].gsub(/[;$&]/, "")}`
+              out = `cd #{ROOT_DIR};git checkout tags/v#{args[0].gsub(/[;$&]/, "")} --force`
               return Packet.new(:response, "Success")
             else
               return Packet.new(:error, "Invalid command")
