aec5b814a5cb30b306c2886f4fef04f2cb6a0ad2
Matthias Lee <matthias@matthiasclee.com>
2023-02-12 12:24:42 -0800

    Added dvcauth

--- scripts/config.rb
+++ scripts/config.rb
@@ -2,6 +2,7 @@ require "argparse"
 require "json"
 require "socket"
 require_relative "../lib/visiontest/vtac/packet.rb"
+require_relative "../lib/visiontest/dvc/packet.rb"
 
 o = {
   load: { has_argument: true },
@@ -44,6 +45,8 @@ if args.options[:save] # Export data
   data[:version] = id_server[1]
   data[:auth] = id_server[2] ? true : false
 
+  # Config options
+
   data[:config] = {}
 
   server.puts VisionTest::VTAC::Packet.new(:command, "config pxsize")
@@ -55,5 +58,20 @@ if args.options[:save] # Export data
   server.puts VisionTest::VTAC::Packet.new(:command, "config mirrored")
   data[:config][:mirrored] = VisionTest::VTAC::Packet.new(from_packet: server.gets.chomp)[:contents].to_i
 
-  puts data.to_s
+  # DVC
+
+  server.puts VisionTest::VTAC::Packet.new(:command, "dvcenable")
+  data[:dvcenabled] = (VisionTest::VTAC::Packet.new(from_packet: server.gets.chomp)[:contents] == "true" ? true : false)
+
+  if data[:dvcenabled]
+    server_dvc = TCPSocket.new(args.data[0], 5583)
+    id_server_dvc = VisionTest::DVC::Packet.new(from_packet: server_dvc.gets.chomp)[:contents].split("~")
+    server_dvc.close
+
+    data[:dvcauth] = id_server_dvc[2] ? true : false
+  else
+    data[:dvcauth] = false
+  end
+
+  puts data.to_json
 end
