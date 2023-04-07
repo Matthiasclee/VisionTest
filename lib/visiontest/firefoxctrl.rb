5529d68327394486c94b7eaba98f0e6b575c2ca0
Matthias Lee <matthias@matthiasclee.com>
2023-04-07 07:01:04 -0700

    Change user agent

--- lib/visiontest/firefoxctrl.rb
+++ lib/visiontest/firefoxctrl.rb
@@ -2,7 +2,10 @@ module VisionTest
   module FirefoxCtrl
     @driver=nil
     def self.start_driver
-      options = Selenium::WebDriver::Firefox::Options.new(args: ['--kiosk'])
+      user_agent = 'Mozilla/5.0 (SMART-TV; Linux) AppleWebkit/605.1.15 (KHTML, like Gecko) SamsungBrowser/9.2 TV Sa'
+      profile = Selenium::WebDriver::Firefox::Profile.new
+      profile['general.useragent.override'] = user_agent
+      options = Selenium::WebDriver::Firefox::Options.new(args: ['--kiosk'], profile: profile)
       @driver = Selenium::WebDriver.for :firefox, capabilities: options
       @driver.navigate.to 'file://' + ROOT_DIR + '/table.html'
     end
