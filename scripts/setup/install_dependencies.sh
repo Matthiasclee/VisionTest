f64a50328856b4bebe00142b61a0a8bc7e4b8e48
Matthias Lee <matthias@matthiasclee.com>
2023-04-07 06:21:43 -0700

    Add --gem-no-sudo option to install dependencies script

--- scripts/setup/install_dependencies.sh
+++ scripts/setup/install_dependencies.sh
@@ -5,5 +5,10 @@ echo "Installing packages (you may be asked for your password)"
 sudo apt-get install ruby ruby-dev gcc make automake git firefox-geckodriver unclutter libgmp-dev libgmp3-dev libpcre3 libpcre3-dev -y
 
 echo "Installing gems (you may be asked for your password)"
-sudo gem install selenium-webdriver ffi rbtext serialport json
+
+if [ "$1" == "--gem-no-sudo" ]; then
+  gem install selenium-webdriver ffi rbtext serialport json
+else
+  sudo gem install selenium-webdriver ffi rbtext serialport json
+fi
 
