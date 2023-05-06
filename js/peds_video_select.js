20e1e5a1d8011ca7af823460b5079f1f92be5c30
Matthias Lee <matthias@matthiasclee.com>
2023-05-06 08:18:27 -0700

    Add up and down functions to video

---
+++ js/peds_video_select.js
@@ -0,0 +1,25 @@
+let selectedvideo=1
+
+function up(){
+  if (selectedvideo == 1){
+    document.getElementById(1).classList.remove("selected") 
+    document.getElementById(3).classList.add("selected") 
+    selectedvideo = 3
+  } else {
+    selectedvideo = selectedvideo - 1
+    document.getElementById(selectedvideo+1).classList.remove("selected") 
+    document.getElementById(selectedvideo).classList.add("selected") 
+  }
+}
+
+function down(){
+  if (selectedvideo == 3){
+    document.getElementById(3).classList.remove("selected") 
+    document.getElementById(1).classList.add("selected") 
+    selectedvideo = 1
+  } else {
+    selectedvideo = selectedvideo + 1
+    document.getElementById(selectedvideo-1).classList.remove("selected") 
+    document.getElementById(selectedvideo).classList.add("selected") 
+  }
+}
