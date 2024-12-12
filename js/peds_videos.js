d94a9db19410639ff8242175595d33626de7fce1
Matthias Lee <matthias@matthiasclee.com>
2024-12-11 19:34:08 -0800

    Change video playback

--- js/peds_videos.js
+++ js/peds_videos.js
@@ -1,10 +1,11 @@
 let selectedvideo=1
+let selectedvideomax=3
 
 function svup(){
   if (selectedvideo == 1){
     document.getElementById(1).classList.remove("selected") 
-    document.getElementById(3).classList.add("selected") 
-    selectedvideo = 3
+    document.getElementById(selectedvideomax).classList.add("selected") 
+    selectedvideo = selectedvideomax
   } else {
     selectedvideo = selectedvideo - 1
     document.getElementById(selectedvideo+1).classList.remove("selected") 
@@ -13,8 +14,8 @@ function svup(){
 }
 
 function svdown(){
-  if (selectedvideo == 3){
-    document.getElementById(3).classList.remove("selected") 
+  if (selectedvideo == selectedvideomax){
+    document.getElementById(selectedvideomax).classList.remove("selected") 
     document.getElementById(1).classList.add("selected") 
     selectedvideo = 1
   } else {
@@ -26,4 +27,12 @@ function svdown(){
 
 function getSelectedVideo(){
   return selectedvideo
+}
+
+function start_video(){
+  video = document.getElementById("videofeed")
+  video_name = location.href.split("#")[1] 
+
+  video.src = `videos/${video_name}`
+  video.play()
 }
