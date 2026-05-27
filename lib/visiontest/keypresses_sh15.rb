e7294e521986c26a9aed8df80c2d37a6e4c56382
Matthias Lee <matthias@matthiasclee.com>
2026-05-27 08:15:06 -0700

    Add programming buttons

--- lib/visiontest/keypresses_sh15.rb
+++ lib/visiontest/keypresses_sh15.rb
@@ -1,6 +1,9 @@
 module VisionTest
   module KeypressesSH15
     active_video_re = /.*html\/playvideo.html\#video[0-9]+\.mp4/
+    programming_mode = :off
+    #                  :calibration
+    #                  :roomlength
 
     Serial.oncode 'sh15A8' do
       `amixer set Master on`
@@ -16,7 +19,14 @@ module VisionTest
     Serial.oncode 'sh15A2' do
       case FirefoxCtrl.page
       when "table.html"
-        FirefoxCtrl.driver.execute_script 'up()'
+        case programming_mode
+        when :calibration
+          Calibration.changePxSize(1)
+        when :roomlength
+          Calibration.changeRoomLength(1)
+        else
+          FirefoxCtrl.driver.execute_script 'up()'
+        end
       when "pediatricsvideos.html"
         FirefoxCtrl.driver.execute_script 'svup()'
       end
@@ -25,7 +35,14 @@ module VisionTest
     Serial.oncode 'sh15A6' do
       case FirefoxCtrl.page
       when "table.html"
-        FirefoxCtrl.driver.execute_script 'down()'
+        case programming_mode
+        when :calibration
+          Calibration.changePxSize(-1)
+        when :roomlength
+          Calibration.changeRoomLength(-1)
+        else
+          FirefoxCtrl.driver.execute_script 'down()'
+        end
       when "pediatricsvideos.html"
         FirefoxCtrl.driver.execute_script 'svdown()'
       end
@@ -34,6 +51,7 @@ module VisionTest
     # Hide button
     Serial.oncode 'sh15AC' do; FirefoxCtrl.driver.execute_script 'tglSizeNum()' end
 
+    # Middle Button
     Serial.oncode 'sh15A4' do
       case FirefoxCtrl.page
       when "table.html"
@@ -69,7 +87,7 @@ module VisionTest
     # Screensaver
     Serial.oncode 'sh15A0' do; FirefoxCtrl.driver.execute_script 'screenSaver()' end
 
-    # Randomize letters
+    # Crowding
     Serial.oncode 'sh15AB' do; FirefoxCtrl.driver.execute_script 'toggleSymCrowd(); fill(cSize)' end
 
     # Mode buttons
@@ -89,5 +107,37 @@ module VisionTest
 
     # SGL
     Serial.oncode 'sh15AA' do; FirefoxCtrl.driver.execute_script 'tglSingle()' end
+
+    ## Programming
+    Serial.oncode 'sh15AD' do
+      commands_path = "/media/#{ENV['USER']}/VT/run_commands.vt"
+
+      if File.exist?(commands_path)
+        commands = File.read(commands_path).split("\n")
+        commands.each {|c| VTAC::Server::Commands.run(c)}
+      end
+    end
+
+    Serial.oncode 'sh15AE' do
+      if File.exist?("/media/#{ENV['USER']}/VT/vtcalib")
+        if programming_mode == :off # Enter calibration mode
+          programming_mode = :calibration
+          FirefoxCtrl.driver.execute_script 'toggleSizeOf20();'
+          FirefoxCtrl.driver.execute_script 'fill("calibration")'
+
+        elsif programming_mode == :calibration # Enter roomlength mode
+          programming_mode = :roomlength
+
+          FirefoxCtrl.driver.execute_script 'toggleSizeOf20();'
+          FirefoxCtrl.driver.execute_script 'fill(1, false, true)'
+
+          FirefoxCtrl.driver.execute_script 'toggleRoomLength();'
+        elsif programming_mode == :roomlength # Turn programming off
+          programming_mode = :off
+
+          FirefoxCtrl.driver.execute_script 'toggleRoomLength();'
+        end
+      end
+    end
   end
 end
