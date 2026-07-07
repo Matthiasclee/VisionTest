60168676892e7409aca81cc6f5a88c51ae3ce4c6
Matthias Lee <matthias@matthiasclee.com>
2026-07-06 20:58:36 -0700

    Git clone from USB

--- scripts/linux_install.sh
+++ scripts/linux_install.sh
@@ -44,7 +44,8 @@ read receiver_tty
 
 echo "Downloading VisionTest"
 cd ~
-git clone https://github.com/MatthiasCLee/VisionTest.git
+# git clone https://github.com/MatthiasCLee/VisionTest.gitc
+git clone /media/$USER/vt/VisionTest
 
 echo "Creating config file"
 echo "$receiver_tty" > ~/.vtreceiverport
